f5d541ace20466553321c3e04e551d8adbb4eae6
TsurumaruTsuyoshi <192761079+changchinlan@users.noreply.github.com>
2026-08-26 12:20:53 +0800

    build: package deb with host fpm

--- build.sh
+++ build.sh
@@ -8,22 +8,20 @@ ldflags="$(cat release/LDFLAGS)"
 podman run --rm \
   -v "$(pwd)":/root/sing-box \
   -w /root/sing-box \
-  golang:bookworm bash -c "
-    set -e
-    go build -v -trimpath -mod=mod \
-      -o dist/sing-box \
-      -tags '${tags}' \
-      -ldflags '-s -w -buildid= -X github.com/sagernet/sing-box/constant.Version=v${version} ${ldflags}' \
-      ./cmd/sing-box
+  golang:bookworm \
+  go build -v -trimpath -mod=mod \
+    -o dist/sing-box \
+    -tags "$tags" \
+    -ldflags "-s -w -buildid= -X github.com/sagernet/sing-box/constant.Version=v${version} ${ldflags}" \
+    ./cmd/sing-box
 
-    apt-get update -qq && apt-get install -y -qq ruby-full >/dev/null
-    gem install --no-document fpm
-    cp .fpm_systemd .fpm
-    fpm -t deb \
-      -v '${version}' \
-      -p 'dist/sing-box_${version}_debian_amd64.deb' \
-      --architecture amd64 \
-      dist/sing-box=/usr/bin/sing-box
-  "
+command -v fpm >/dev/null
+cp .fpm_systemd .fpm
+rm -f "dist/sing-box_${version}_debian_amd64.deb"
+fpm -t deb \
+  -v "$version" \
+  -p "dist/sing-box_${version}_debian_amd64.deb" \
+  --architecture amd64 \
+  dist/sing-box=/usr/bin/sing-box
 
 echo "done: dist/sing-box_${version}_debian_amd64.deb"
